524fabc2810bbd09b78032bbafd382288037c381
wedersonals <wederson_adriano@hotmail.com>
2022-02-26 10:45:14 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/java/br/wals/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/br/wals/hrpayroll/resources/PaymentResource.java
@@ -2,6 +2,7 @@ package br.wals.hrpayroll.resources;
 
 import br.wals.hrpayroll.entities.Payment;
 import br.wals.hrpayroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,9 +19,15 @@ public class PaymentResource {
         this.service = service;
     }
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping("/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable("workerId") Long workerId,
                                               @PathVariable("days") int days) {
         return ResponseEntity.ok(service.getPayment(workerId, days));
+    }
+
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerId, int days) {
+        Payment payment = new Payment("Bran", 400.0, days);
+        return ResponseEntity.ok(payment);
     }
 }
