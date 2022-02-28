254c7688fafb218698e51372222dfbc1431757a3
wedersonals <wederson_adriano@hotmail.com>
2022-02-28 19:30:40 -0300

    UserResource refactored

--- hr-user/src/main/java/br/wals/hruser/resources/UserResource.java
+++ hr-user/src/main/java/br/wals/hruser/resources/UserResource.java
@@ -2,14 +2,8 @@ package br.wals.hruser.resources;
 
 import br.wals.hruser.entities.User;
 import br.wals.hruser.repositories.UserRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/users")
