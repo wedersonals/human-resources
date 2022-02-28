aa318ab5aae03e11d03226735353a477547927cc
wedersonals <wederson_adriano@hotmail.com>
2022-02-28 07:37:23 -0300

    User, role, many to many association

---
+++ hr-user/src/main/java/br/wals/hruser/entities/Role.java
@@ -0,0 +1,26 @@
+package br.wals.hruser.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name = "tb_role")
+public class Role implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String roleName;
+
+}
